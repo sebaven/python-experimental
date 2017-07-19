9837d312dcefdb333472d2eb904facfc718fb414
sebastien <sebastien.venot@gmail.com>
2017-07-19 17:36:33 +0300

    cleanup

--- sample-programs/farhenheit.py
+++
@@ -1,4 +0,0 @@
-var = raw_input("Please enter temperature in Celsius: ")
-print ("you entered: " + var + "C")
-farhenheit = (int(var) * 1.8) + 32
-print ("Value in Farhenheit: " + str(farhenheit)+ "F")

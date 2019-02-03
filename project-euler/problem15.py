bc2c42d51de59fc3bf7e68c674d4a54dc17279f0
sebastien <sebastien.venot@gmail.com>
2019-02-03 21:51:10 +0200

    resolved euler problem 15

--- project-euler/problem15.py
+++ project-euler/problem15.py
@@ -1,9 +1,27 @@
 """
 lattice paths
-Starting in the top left corner of a 2×2 grid, and only being able to move to the right and down,
+Starting in the top left corner of a 2x2 grid, and only being able to move to the right and down,
 there are exactly 6 routes to the bottom right corner.
-
-How many such routes are there through a 20×20 grid?
+How many such routes are there through a 20x20 grid?
 """
 
-# TODO
+""" 
+Never heard of Lattice paths before
+some study was needed but a good introduction on the formula is described here:
+https://www.robertdickau.com/manhattan.html
+paths = (2n)!/(n!^2)
+"""
+
+n = 20
+
+a = 1
+for i in range(1, (2 * n) +1):
+    a = a * i
+
+b = 1
+for j in range(1, n+1):
+    b = b * j
+
+paths = a / (b * b)
+print ("Amount of paths through a 20x20 grid: %s" % paths)
+
